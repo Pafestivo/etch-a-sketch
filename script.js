5d9c7679a4bd44a94dc3cbc50df234d84e868069
Or <orrodrigez1@gmail.com>
2022-10-16 14:56:21 +0300

    Add an option to change the canvas size

--- script.js
+++ script.js
@@ -1,34 +1,63 @@
 const gridBox = document.getElementById('grid'); //target the grid container
 
-function createBoxes() {
-  for (i = 0; i < (16 * 16); i++) {
-    const createBox = document.createElement('div'); //create new div
-    createBox.classList.add('inner-grid');
-    createBox.setAttribute('id', 'box' + i);
-    gridBox.appendChild(createBox);
 
-    const innerGrid = document.getElementById('box' + i);
-    innerGrid.addEventListener('mouseover', colorChange);
 
-    function colorChange() {
-    innerGrid.classList.add('red-background');
+
+//choose size button
+const chooseSize = document.getElementById('choose-size');
+chooseSize.addEventListener('click', sizePicker);
+
+function sizePicker() {
+  
+  const playerInput = prompt('Choose a number(1-64)');
+  if(playerInput > 64) {
+    alert("Please choose a number between 1-64");
+    return;
+  }
+
+  function removeGrid() {
+    while (gridBox.firstChild) {
+      gridBox.removeChild(gridBox.firstChild);
     }
   }
-}
+  removeGrid();
 
-//clear button
-const clear = document.getElementById('btn'); 
-clear.addEventListener('click', clearColors);
+  gridBox.style.gridTemplateColumns = 'repeat(' + playerInput + ', 1fr)';
 
-function clearColors() {
-  for (i = 0; i < (16 * 16); i++) {
-    const innerGrid = document.getElementById('box' + i);
-    innerGrid.classList.remove('red-background');
+  function createBoxes() {
+    for (i = 0; i < (playerInput * playerInput); i++) {
+      const createBox = document.createElement('div'); //create new div
+      createBox.classList.add('inner-grid');
+      createBox.setAttribute('id', 'box' + i);
+      gridBox.appendChild(createBox);
+  
+      const innerGrid = document.getElementById('box' + i);
+      innerGrid.addEventListener('mouseover', colorChange);
+  
+      function colorChange() {
+      innerGrid.classList.add('red-background');
+      }
+    }
   }
+  createBoxes();
+  
+  //clear button
+  const clear = document.getElementById('clear'); 
+  clear.addEventListener('click', clearColors);
+  
+  function clearColors() {
+    for (i = 0; i < (playerInput * playerInput); i++) {
+      const innerGrid = document.getElementById('box' + i);
+      innerGrid.classList.remove('red-background');
+    }
+  }
+  //end clear button
+  
 }
-//end clear button
 
-createBoxes();
+//end choose size button
+
+
 
 
 
