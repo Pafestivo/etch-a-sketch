9ff63ac3a7c6d04f4b99406790fb2029ca040dbc
Or <orrodrigez1@gmail.com>
2022-10-16 20:09:03 +0300

    Define all the html selectors

---
+++ script.js
@@ -0,0 +1,12 @@
+const DEFAULT_COLOR = '#000';
+const DEFAULT_MODE = 'color';
+const DEFAULT_SIZE = 16;
+
+const grid = document.getElementById('grid');
+const colorMode = document.getElementById('color-mode');
+const rainbowMode = document.getElementById('rainbow-mode');
+const eraseBtn = document.getElementById('erase');
+const clear = document.getElementById('clear');
+const slideValue = document.getElementById('slider-value');
+const sliderSize = document.getElementById('size-slider');
+const colorPicker = document.getElementById('color-picker');
