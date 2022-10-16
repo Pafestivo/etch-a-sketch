053ceac411d40367213f1b2de78742c3e6c5e403
Or <orrodrigez1@gmail.com>
2022-10-16 20:47:38 +0300

    Set the function to create the grid

--- script.js
+++ script.js
@@ -2,11 +2,45 @@ const DEFAULT_COLOR = '#000';
 const DEFAULT_MODE = 'color';
 const DEFAULT_SIZE = 16;
 
+let currentColor = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_SIZE;
+
+
+function setCurrentColor(newColor) {
+  currentColor = newColor
+}
+
+function setCurrentMode(newMode) {
+  currentMode = newMode
+}
+
+function setCurrentSize(newSize) {
+  currentSize = newSize
+}
+
+
 const grid = document.getElementById('grid');
 const colorMode = document.getElementById('color-mode');
 const rainbowMode = document.getElementById('rainbow-mode');
 const eraseBtn = document.getElementById('erase');
 const clear = document.getElementById('clear');
-const slideValue = document.getElementById('slider-value');
-const sliderSize = document.getElementById('size-slider');
+const sliderValue = document.getElementById('slider-value');
+const sizeSlider = document.getElementById('size-slider');
 const colorPicker = document.getElementById('color-picker');
+
+
+createGrid(currentSize);
+
+
+function createGrid(size) {
+  grid.style.gridTemplateColumns = 'repeat(' + size + ', 1fr)';
+
+  for(i = 0; i < size * size; i++) {
+    const innerBox = document.createElement('div')
+    innerBox.setAttribute('id', 'box' + i)
+    innerBox.classList.add('innerBox');
+    grid.appendChild(innerBox);
+  }
+
+}
